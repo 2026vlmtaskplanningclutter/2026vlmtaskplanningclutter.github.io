533f9e784e00bdaf6eb77a3cbd4c739db7bd868a
Ghost <Ghost@users.noreply.github.com>
2025-12-09 09:32:15 -0500

    Initial commit

---
+++ anonymize.sh
@@ -0,0 +1,7 @@
+git filter-repo --force --commit-callback '
+  commit.author_name = b"Ghost"
+  commit.author_email = b"Ghost@users.noreply.github.com"
+  commit.committer_name = b"Ghost"
+  commit.committer_email = b"Ghost@users.noreply.github.com"
+'
+git remote add origin https://github.com/2026vlmtaskplanningclutter/2026vlmtaskplanningclutter.github.io
