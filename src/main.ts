211ef38427091654b99ae1b7801247a1620d1063
Ghost <Ghost@users.noreply.github.com>
2025-12-10 00:03:04 -0500

    Add redirects for common paths

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,22 @@ initComponents();
 document.getElementById("vlm-prompt")!.innerText = promptPickObject;
 document.getElementById("vlm-dg-prompt")!.innerText = promptMakeDg;
 document.getElementById("dg-vlm-prompt")!.innerText = promptPickObjectDgDesc;
+
+switch (window.location.pathname) {
+  case "/scenes":
+    document.getElementById("scenes")?.scrollIntoView({
+      behavior: "smooth",
+    });
+    break;
+  case "/methods":
+  case "/prompts":
+    document.getElementById("methods")?.scrollIntoView({
+      behavior: "smooth",
+    });
+    break;
+  case "/bibtex":
+    document.getElementById("bibtex")?.scrollIntoView({
+      behavior: "smooth",
+    });
+    break;
+}
